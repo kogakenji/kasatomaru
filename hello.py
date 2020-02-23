ffb3d2909830cd0f3194c5710f052c44beedbec7
Kenji Koga <koga.k3nj1@gmail.com>
2020-02-23 13:20:10 +1100

    Added 1 2 3

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ print("Let's Go!")
 print("Hello World!")
 
 def test():
-    return "Coming to a function..."
+    return "Coming to a function... 1 2 3"
 
 if __name__ == "__main__":
     print(test())
