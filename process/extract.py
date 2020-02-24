0619edb7500a0e21b30ba574df6f29efe0f86d34
Kenji Koga <koga.k3nj1@gmail.com>
2020-02-24 21:03:26 +1100

    Processing data
    Extracts data from html page file

--- process/extract.py
+++ process/extract.py
@@ -13,27 +13,32 @@ def files_list(start, end):
 
 if __name__ == "__main__":
     # to_extract = files_list(1, 49276)
-    pages = files_list(1, 5)
-    print(pages)
+    pages = files_list(1, 49276)
+    print(len(pages))
     for page in pages:
         path = pathlib.Path.cwd().parent / "crawler" / page
         print(path)
         with open(str(path), encoding="ISO-8859-1") as p:
             # print(p.read())
-            # soup = BeautifulSoup(p.read(), 'html.parser')
-            soup = BeautifulSoup(p.read(), 'lxml')
+            soup = BeautifulSoup(p.read(), 'html.parser')
+            # soup = BeautifulSoup(p.read(), 'lxml')
 
-            table = soup.find_all("table", bgcolor="#FFFFFF")
+            # table = soup.find_all("table", bgcolor="#FFFFFF")
+            # print(table)
 
             data = soup.find_all("tr",{'class':'texto'})
 
-            print(data)
-            print(type(data))
-            print(len(data))
-            # for item in data:
-            #     soup_tr = BeautifulSoup(item.text, "lxml")
-            #     tds = soup.find_all("td")
-            #     count = 0
-            #     for td in tds:
-            #         print(count, td.text)
-            #         count += 1
+            for i, d in enumerate(data):
+                tds = data[i].find_all('td')
+
+                ship = tds[0].a.contents[0]
+                leave_date = tds[1].a.contents[0]
+                link_family = tds[1].a.get("href")
+                arrive_date = tds[1].a.contents[2]
+                province = tds[2].a.contents[0]
+                destination = tds[3].a.contents[0]
+                surname = tds[4].a.contents[0]
+                name = tds[5].a.contents[0]
+                print(f"Ship: {ship} - leave_date: {leave_date} - arrive_date: {arrive_date} - province: {province} - destination: {destination} - surname: {surname} - name: {name}")
+                print(f"link_family: {link_family}")
+        break
